26afe335741a689530444a48a5bda47ef254d4e4
ismtbkhatri <binod.khatri@ismt.edu.np>
2026-06-04 07:46:47 +0545

    implementing Custom validator

---
+++ BlazorApp1/CustomValidation/CustomValidator.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.CustomValidation
+{
+    public class CustomValidator : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (string.IsNullOrEmpty(value.ToString()))
+                return new ValidationResult("Failed to pass custom validation" );
+            // Custom validation logic here
+            return ValidationResult.Success;
+        }
+    }
+}
+
+
